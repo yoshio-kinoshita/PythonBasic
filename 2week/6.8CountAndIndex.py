20914755c4673776797cc59d61bec0273b37ecc8
Yoshio Kinoshita <yoshio.kinoshita@icloud.com>
2019-01-12 18:47:10 +0900

    file name change

---
+++ 2week/6.8CountAndIndex.py
@@ -0,0 +1,12 @@
+a = "I have had an apple on my desc before!"
+print(a.count('e'))
+print(a.count('ha'))
+
+music = "Pull out your music and dancing can begin"
+bio = ["Metatarsal", "Metatarsal", "Fibula", [], "Tibia", "Tibia", 43, "Femur", "Occipital", "Metatarsal"]
+print(music.index("m"))
+print(music.index("your"))
+
+print(bio.index("Metatarsal"))
+print(bio.index([]))
+print(bio.index(43))
