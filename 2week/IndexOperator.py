d3e9844f720ee4ad5acea09feaf6bd26a6da537c
Yoshio Kinoshita <yoshio.kinoshita@icloud.com>
2019-01-12 17:12:20 +0900

    first commit
    add new files

---
+++ 2week/IndexOperator.py
@@ -0,0 +1,14 @@
+school = "Luther Coller"
+
+m = school[2]
+print(m)
+
+lastchar = school[-1]
+print(lastchar)
+
+numbers = [17, 123, 87, 34, 66, 8398, 44]
+print(numbers[2])
+print(numbers[9-8])
+print(numbers[-2])
+print(numbers[-0])
+print(numbers[0])
