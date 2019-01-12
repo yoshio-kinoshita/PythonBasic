9fadc928fbb60d20b5f52a21eeab4887ef67aece
Yoshio Kinoshita <yoshio.kinoshita@icloud.com>
2019-01-12 18:35:03 +0900

    add files

---
+++ 2week/6.5Length.py
@@ -0,0 +1,11 @@
+fruit = "Banana"
+print(len(fruit))
+
+alist = ["hello", 2.0, 5]
+print(len(alist))
+print(len(alist[0]))
+
+l = [0.34, '6', 'SI106', 'python', -2]
+print(len(l[1:-1]))
+print(l[1:])
+print(l[-1])
---
+++ 2week/6.6.TheSliceOperator.py
@@ -0,0 +1,4 @@
+singers = "Petter, Paul, and Mary"
+print(singers[0:5])
+print(singers[7:11])
+print(singers[17:21])
